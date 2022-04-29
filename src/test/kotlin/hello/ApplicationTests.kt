d8b9bfd14c3c9ee184b385d171006eaa4b0b298f
Prutha Nagrikar <41024156+pruthanagrikar@users.noreply.github.com>
2022-04-29 11:46:37 +0200

    Update ApplicationTests.kt

--- src/test/kotlin/hello/ApplicationTests.kt
+++ src/test/kotlin/hello/ApplicationTests.kt
@@ -14,10 +14,5 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
 class ApplicationTests(@Autowired private val restTemplate: TestRestTemplate) {
 
-	@Test
-	fun findAll() {
-		val content = """[{"firstName":"Jack","lastName":"Bauer","id":1},{"firstName":"Chloe","lastName":"O'Brian","id":2},{"firstName":"Kim","lastName":"Bauer","id":3},{"firstName":"David","lastName":"Palmer","id":4},{"firstName":"Michelle","lastName":"Dessler","id":5}]"""
-		assertEquals(content, restTemplate.getForObject<String>("/customers"))
-	}
 
 }
