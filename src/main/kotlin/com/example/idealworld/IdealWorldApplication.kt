6a52b0984cf41635336d8ad5d1d2882edf50b831
Prutha Nagrikar <41024156+pruthanagrikar@users.noreply.github.com>
2022-04-29 11:53:38 +0200

    Add files via upload

---
+++ src/main/kotlin/com/example/idealworld/IdealWorldApplication.kt
@@ -0,0 +1,11 @@
+package com.example.idealworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdealWorldApplication
+
+fun main(args: Array<String>) {
+	runApplication<IdealWorldApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/idealworld/IdealWorldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.idealworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdealWorldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
